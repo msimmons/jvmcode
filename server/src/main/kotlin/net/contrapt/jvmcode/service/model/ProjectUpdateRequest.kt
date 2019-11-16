83e26a43a87780f9fb0686b87175bbac213f5767
Mark Simmons <msimmonz@gmail.com>
2019-11-16 17:23:36 -0500

    Concrete types

--- server/src/main/kotlin/net/contrapt/jvmcode/service/model/ProjectUpdateRequest.kt
+++ server/src/main/kotlin/net/contrapt/jvmcode/service/model/ProjectUpdateRequest.kt
@@ -1,12 +1,10 @@
 package net.contrapt.jvmcode.service.model
 
-import net.contrapt.jvmcode.model.ClasspathData
-import net.contrapt.jvmcode.model.DependencySourceData
 import net.contrapt.jvmcode.model.ProjectUpdateData
 
 class ProjectUpdateRequest(
         override val source: String,
-        override val dependencySources: Collection<DependencySourceData>,
-        override val classDirs: Collection<ClasspathData>
+        override val dependencySources: Collection<DependencySource>,
+        override val classDirs: Collection<Classpath>
 
 ) : ProjectUpdateData
